f9be161929e7064b04f7f6959db51f450889876e
Han-nahh <hannatesfaye11@gmail.com>
2022-04-28 20:53:53 +0300

    apr 28

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,12 +2,13 @@
 
 /*Prints out a string followed by a new line*/
 
-void _puts_recursion(char *s){
-	if(*s=='\o'){
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
 		_putchar('\n');
-return;
-}
-_putchar(*s);
-_puts_recursion(s++);
-		
 }
